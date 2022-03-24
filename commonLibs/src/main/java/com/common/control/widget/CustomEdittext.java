6d87fb00626ba2bb8d33439fc4b819c5c3c6ac63
datcorona <dat.corona97@gmail.com>
2022-03-24 23:42:54 +0700

    update widget

--- commonLibs/src/main/java/com/common/control/widget/CustomEdittext.java
+++ commonLibs/src/main/java/com/common/control/widget/CustomEdittext.java
@@ -1,9 +1,11 @@
 package com.common.control.widget;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.KeyEvent;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatEditText;
 
 public class CustomEdittext extends AppCompatEditText {
@@ -11,9 +13,17 @@ public class CustomEdittext extends AppCompatEditText {
         super(context);
     }
 
+    public CustomEdittext(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomEdittext(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
     private OnHideKeyboardListener mOnHideKeyboardListener;
 
-    public void setmOnHideKeyboardListener(OnHideKeyboardListener mOnHideKeyboardListener) {
+    public void setOnHideKeyboardListener(OnHideKeyboardListener mOnHideKeyboardListener) {
         this.mOnHideKeyboardListener = mOnHideKeyboardListener;
     }
 
--- commonLibs/src/main/java/com/common/control/widget/CustomViewPager.java
+++ commonLibs/src/main/java/com/common/control/widget/CustomViewPager.java
@@ -1,15 +1,22 @@
 package com.common.control.widget;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.viewpager.widget.ViewPager;
 
 public class CustomViewPager extends ViewPager {
     public CustomViewPager(@NonNull Context context) {
         super(context);
     }
+
+    public CustomViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
     private boolean isPagingEnabled;
 
     @Override
